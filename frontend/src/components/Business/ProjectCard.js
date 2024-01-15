0a1931518252a6c1e4de328112a3271f69bd7274
Dylan <dylanquach@gmail.com>
2024-01-14 23:07:46 -0800

    Remove unneccesarry

--- frontend/src/components/Business/ProjectCard.js
+++ frontend/src/components/Business/ProjectCard.js
@@ -1,17 +1,17 @@
-const ProjectCard = ({url}) => {
-  return (
-    <div
-      className="projectCard"
-      style={{
-        backgroundImage: `url(${url})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <h1>Whale Gaming</h1>
-      <h2>Top gaming Information</h2>
-    </div>
-  );
-};
+// const ProjectCard = ({url}) => {
+//   return (
+//     <div
+//       className="projectCard"
+//       style={{
+//         backgroundImage: `url(${url})`,
+//         backgroundSize: "cover",
+//         backgroundPosition: "center",
+//       }}
+//     >
+//       <h1>Whale Gaming</h1>
+//       <h2>Top gaming Information</h2>
+//     </div>
+//   );
+// };
 
-export default ProjectCard;
+// export default ProjectCard;
