85318ea6125cc8e6d12670d21c3a8bbc7205251f
Dylan <dylanquach@gmail.com>
2024-01-24 19:24:59 -0800

    setup aws

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  config.active_storage.service = :test
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
