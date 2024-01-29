feeebda78e5dc2be8d915ff0d419ab88528ed2d7
Dylan <dylanquach@gmail.com>
2024-01-29 10:04:33 -0800

    Seed reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -445,158 +445,94 @@ r1 = Review.create!(
   rating: 5,
   business_id: 1,
   user_id: 2,
-  body: "Pretty good.",
+  body: "Very cheesy, I love it!.",
   images: [
     { io: URI.open("https://dine-discovery-seeds.s3.us-west-1.amazonaws.com/freshly-italian-pizza-with-mozzarella-cheese-slice-generative-ai_188544-12347.avif"), filename: "image1.jpg" }
-    # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee2.jpg"), filename: "image2.jpg" },
-    # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee3.jpg"), filename: "image3.jpg" },
   ],
 )
-Business.find(wills_1.business_id).update_average_rating
+Business.find(r1.business_id).update_average_rating
 
+r2 = Review.create!(
+  rating: 2,
+  business_id: 10,
+  user_id: 3,
+  body: "Too sweet for my liking",
+  images: [
+  ],
+)
+Business.find(r2.business_id).update_average_rating
 
-# wills_2 = Review.create!(
-#   rating: 5,
-#   business_id: 10,
-#   user_id: 2,
-#   body: "Bro what is this trash.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee3.jpg"), filename: "image4.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee4.jpg"), filename: "image5.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee5.jpg"), filename: "image6.jpg" },
-#   ],
-# )
-# Business.find(wills_2.business_id).update_average_rating
+r3 = Review.create!(
+  rating: 4,
+  business_id: 17,
+  user_id: 4,
+  body: "Amazing burgers and fries #tasty",
+  images: [
+    { io: URI.open("https://dine-discovery-seeds.s3.us-west-1.amazonaws.com/burg.jpg"), filename: "burg.jpg" }
+  ],
+)
+Business.find(r3.business_id).update_average_rating
 
-# wills_3 = Review.create!(
-#   rating: 5,
-#   business_id: 11,
-#   user_id: 3,
-#   body: "I love it!.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee1.jpg"), filename: "image4.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee11.jpg"), filename: "image5.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee10.jpg"), filename: "image6.jpg" },
-#   ],
-# )
-# Business.find(wills_3.business_id).update_average_rating
+r4 = Review.create!(
+  rating: 5,
+  business_id: 10,
+  user_id: 5,
+  body: "Very refresing!",
+  images: [
+    { io: URI.open("https://dine-discovery-seeds.s3.us-west-1.amazonaws.com/coff.jpg"), filename: "coff.jpg" }
+  ],
+)
+Business.find(r4.business_id).update_average_rating
 
-# wills_4 = Review.create!(
-#   rating: 5,
-#   business_id: 11,
-#   user_id: 6,
-#   body: "Meh...",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee7.jpg"), filename: "image4.jpg" },
+r5 = Review.create!(
+  rating: 2,
+  business_id: 2,
+  user_id: 6,
+  body: "About what youd expect, not great service",
+  images: [
+  ],
+)
+Business.find(r5.business_id).update_average_rating
 
-#   ],
-# )
-# Business.find(wills_4.business_id).update_average_rating
+r6 = Review.create!(
+  rating: 5,
+  business_id: 13,
+  user_id: 6,
+  body: "Tasty!!",
+  images: [
+  ],
+)
+Business.find(r6.business_id).update_average_rating
 
-# wills_5 = Review.create!(
-#   rating: 1,
-#   business_id: 11,
-#   user_id: 7,
-#   body: "So good!",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee5.jpg"), filename: "image6.jpg" },
-#   ],
-# )
-# Business.find(wills_5.business_id).update_average_rating
+r7 = Review.create!(
+  rating: 5,
+  business_id: 18,
+  user_id: 6,
+  body: "A great variety of foods!",
+  images: [
+    { io: URI.open("https://dine-discovery-seeds.s3.us-west-1.amazonaws.com/burg2.jpg"), filename: "burg2.jpg" }
+  ]
+)
+Business.find(r7.business_id).update_average_rating
 
-# wills_6 = Review.create!(
-#   rating: 5,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Mhhh.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee8.jpg"), filename: "image6.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee11.jpg"), filename: "image5.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/coffee10.jpg"), filename: "image9.jpg" },
-#   ],
-# )
-# Business.find(wills_6.business_id).update_average_rating
+r8 = Review.create!(
+  rating: 1,
+  business_id: 3,
+  user_id: 6,
+  body: "Why are they cooking from a cart???",
+  images: [
+    { io: URI.open("https://dine-discovery-seeds.s3.us-west-1.amazonaws.com/badp.jpg"), filename: "badp.jpg" }
 
-# # # end
+  ],
+)
+Business.find(r8.business_id).update_average_rating
 
-# p_1 = Review.create!(
-#   rating: 5,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Kirito, the protagonist of Sword Art Online, is a character who understands the power of prayer. Through his trials and tribulations, he has learned that sometimes the only thing that can sustain us is the strength we draw from a higher power. He has learned to trust in the universe, in God, and in the power of his own faith. And so he prays, not just for himself, but for those he loves and for the world he hopes to save. Kirito knows that prayer is not a magic solution to his problems, but rather a means of finding peace, of centering himself, and of tapping into a greater source of strength. For Kirito, prayer is a reminder that he is never truly alone, and that there is always hope in the darkest of moments.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza9.jpg"), filename: "image1.jpg" },
-
-#   ],
-# )
-# Business.find(p_1.business_id).update_average_rating
-# p_2 = Review.create!(
-#   rating: 5,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Dear God, I come to you today to offer up a prayer for Asuna, a brave and kindhearted warrior from the world of Sword Art Online. Please watch over her and keep her safe as she faces the challenges and dangers of this virtual world. Grant her strength and courage as she fights to protect herself and those she loves. Help her to find peace in the midst of chaos and to hold onto hope even in the darkest of moments. May she know that she is never alone, and that you are always with her, guiding and supporting her along the way. Thank you for blessing us with such a wonderful character, and for reminding us of the power of faith and determination. Amen.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizz3.jpg"), filename: "image4.jpg" },
-
-#   ],
-# )
-# Business.find(p_2.business_id).update_average_rating
-
-# p_3 = Review.create!(
-#   rating: 5,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Saber from Fate/Stay is a truly amazing character. From her striking appearance and regal demeanor to her unwavering sense of justice and selflessness, she embodies all of the qualities that make a hero truly great. She is a skilled warrior and leader, always putting the needs of others before her own, and willing to make the ultimate sacrifice for the greater good. Her unwavering dedication to her ideals, her loyalty to those she cares about, and her willingness to face aCA challenge with courage and determination make her a true inspiration to all who encounter her. Saber is a shining example of what it means to be a true hero, and her legacy will continue to inspire and empower us for generations to come.",
-
-# )
-# Business.find(p_3.business_id).update_average_rating
-
-# p_4 = Review.create!(
-#   rating: 4,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Naruto Uzumaki, the main character of the epoCAmous manga and anime series, is an iconic figure in modern shonen storytelling. He starts out as an underdog, an outcast in his village due to his status as a host for the Nine-Tailed Fox demon. But through his indomitable spirit, hard work, and perseverance, he rises to become one of the most powerful ninja in the world. Naruto's greatest strength lies in his unwavering determination, his ability to never give up even in the face of seemingly insurmountable obstacles. He possesses a heart of gold and a selfless desire to protect and help others, no matter the cost to himself. Naruto's journey is one of growth, both as a ninja and as a person, as he learns to navigate the complexities of the world and the people around him. He is a truly remarkable character, one whose impact on the world of anime and manga is undeniable..",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza11.jpg"), filename: "image9.jpg" },
-
-#   ],
-# )
-# Business.find(p_4.business_id).update_average_rating
-
-# p_5 = Review.create!(
-#   rating: 2,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Sasuke Uchiha, one of the most complex and fascinating characters in the Naruto universe, is a skilled and powerful shinobi with a tragic past. Haunted by the murder of his clan, Sasuke has become consumed by a desire for revenge against his older brother, Itachi. He is often cold and aloof, preferring to keep his emotions hidden beneath a façade of indifference. Despite this, Sasuke is fiercely loyal to those he cares about, especially his former teammate and friend, Naruto Uzumaki. Throughout his journey, Sasuke struggles with his own identity and sense of purpose, constantly questioning his decisions and motivations. But despite his flaws and mistakes, Sasuke remains a compelling and compellingly human character, a testament to the power of storytelling in the Naruto universe.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza7.jpg"), filename: "image5.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza6.jpg"), filename: "image9.jpg" },
-#   ],
-# )
-
-# Business.find(p_5.business_id).update_average_rating
-
-# p_6 = Review.create!(
-#   rating: 3,
-#   business_id: 1,
-#   user_id: 1,
-#   body: "Archer from Fate/stay is a complex and intriguing character. He is a skilled warrior with powerful abilities, but also a tortured soul with a troubled past. Despite his gruff exterior, Archer is deeply introspective and philosophical, often questioning the nature of heroism and the meaning of his own existence. His struggles with his own identity and purpose make him a compelling character to watch, and his conflicts with other characters in the Fate/stay series are always intense and emotional. Archer's abilities as a hero and a warrior are matched only by his depth as a character, making him an unforgettable figure in the world of anime and manga.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza11.jpg"), filename: "image6.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza9.jpg"), filename: "image5.jpg" },
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza10.jpg"), filename: "image9.jpg" },
-#   ],
-# )
-# Business.find(p_6.business_id).update_average_rating
-
-# p_11 = Review.create!(
-#   rating: 2,
-#   business_id: 2,
-#   user_id: 1,
-#   body: "Kirito, the protagonist of Sword Art Online, is a character who understands the power of prayer. Through his trials and tribulations, he has learned that sometimes the only thing that can sustain us is the strength we draw from a higher power. He has learned to trust in the universe, in God, and in the power of his own faith. And so he prays, not just for himself, but for those he loves and for the world he hopes to save. Kirito knows that prayer is not a magic solution to his problems, but rather a means of finding peace, of centering himself, and of tapping into a greater source of strength. For Kirito, prayer is a reminder that he is never truly alone, and that there is always hope in the darkest of moments.",
-#   images: [
-#     # { io: URI.open("https://zelp-seeds.s3.amazonaws.com/pizza9.jpg"), filename: "image1.jpg" },
-
-#   ],
-# )
-# Business.find(p_11.business_id).update_average_rating
+r9 = Review.create!(
+  rating: 5,
+  business_id: 1,
+  user_id: 6,
+  body: "Was better than I thought it would be!",
+  images: [
+  ],
+)
+Business.find(r9.business_id).update_average_rating
