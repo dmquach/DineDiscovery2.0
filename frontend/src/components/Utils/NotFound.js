0848a1b5a3dabf909fa433b1c219a3739737dac7
Dylan <dylanquach@gmail.com>
2024-01-31 00:04:32 -0800

    Fix error page

--- frontend/src/components/Utils/NotFound.js
+++ frontend/src/components/Utils/NotFound.js
@@ -1,11 +1,14 @@
 import "./NotFound.css"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const NotFound =()=>{
     return (
       <div className="cantFind">
         <div>
           <h1>We’re sorry. We can’t find the page you’re looking for.</h1>
-          <h2>Please try a new search</h2>
-
+          <h2>Please try a new </h2>
+          <Link className="signupLink" to="/">
+            search
+          </Link>
         </div>
         <img src="https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_design_cdn/1c54cc25ce01/assets/img/svg_illustrations/cant_find_650x520_v2.svg" alt="" />
       </div>
