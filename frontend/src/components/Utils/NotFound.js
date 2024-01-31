8968d140a72cde6c2b7249aee5ec21e9078b4e3d
Dylan <dylanquach@gmail.com>
2024-01-30 23:58:45 -0800

    Fix not found page:

--- frontend/src/components/Utils/NotFound.js
+++ frontend/src/components/Utils/NotFound.js
@@ -3,8 +3,9 @@ const NotFound =()=>{
     return (
       <div className="cantFind">
         <div>
-          <h1>We’re sorry.</h1>
-          <h1>We can’t find the page you’re looking for.</h1>
+          <h1>We’re sorry. We can’t find the page you’re looking for.</h1>
+          <h2>Please try a new search</h2>
+
         </div>
         <img src="https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_design_cdn/1c54cc25ce01/assets/img/svg_illustrations/cant_find_650x520_v2.svg" alt="" />
       </div>
